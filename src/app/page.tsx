3aaf437e32e543a48fcb7352d6ce0d3df0af3def
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-12-17 12:56:54 +0100

    volviendo a como antes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className='font-bold text-3xl md:text-4xl'>prueba</h1>
+      <h1 className='font-bold text-3xl md:text-4xl'>Tú Feed</h1>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6'>
         {/* Subreddit info y PremiumCard */}
         <div className='space-y-4 order-first md:order-last'>
